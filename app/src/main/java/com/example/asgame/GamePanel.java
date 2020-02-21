985411c227f4241d2d0e652316a02e140bcaf548
Tyson <02tyson.watson@gulllakecs.org>
2020-02-21 09:30:45 -0500

    Draws Player

--- app/src/main/java/com/example/asgame/GamePanel.java
+++ app/src/main/java/com/example/asgame/GamePanel.java
@@ -3,18 +3,23 @@ package com.example.asgame;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
     private MainThread thread;
-    private GamePanel player;
+    private RectPlayer player;
+    private Point playerPoint;
     public GamePanel (Context context){
         super(context);
         getHolder().addCallback(this);
         thread = new MainThread(getHolder(), this);
         setFocusable(true);
+        player = new RectPlayer(new Rect( 100,100,200,200),Color.rgb(255,0,0));
+        playerPoint = new Point(150,150);
     }
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height){
@@ -41,10 +46,12 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
     }
     @Override
     public boolean onTouchEvent(MotionEvent event){
-        return super.onTouchEvent(event);
+        
+        return true;
+        //return super.onTouchEvent(event);
     }
     public void update(){
-
+        player.update(playerPoint);
     }
     @Override
     public void draw(Canvas canvas){
